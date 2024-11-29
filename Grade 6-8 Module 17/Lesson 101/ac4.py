d1cd6487270af6302f8e6346b67ed48f59b7c633
Mimo Patra <90673608+Mimo2k@users.noreply.github.com>
2024-11-29 12:54:12 +0530

    updated Module 16 & 17

---
+++ Grade 6-8 Module 17/Lesson 101/ac4.py
@@ -0,0 +1,9 @@
+number = int(input("Enter number to check"))
+
+print("Number to checked ",number)
+
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")  
---
+++ Grade 6-8 Module 17/Lesson 97/ac1.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+print("My name is Mimo Patra")
+
+print("I love coding")
+
+print("The result of 3 + 5 is ", 3+5)
